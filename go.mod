d1fbb8133f426ff8135addc668e7a832268719b9
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-10-10 11:29:07 +0800

    消息中心gitee采集服务

---
+++ go.mod
@@ -0,0 +1,53 @@
+module github.com/opensourceways/message-collect-githook
+
+go 1.21
+
+require (
+	github.com/opensourceways/community-robot-lib v0.0.0-20220118064921-28924d0a1246
+	github.com/opensourceways/go-gitee v0.0.0-20220714075315-cb246f1dfb96
+	github.com/opensourceways/kafka-lib v0.0.0-20240525094632-94d264f0114c
+	github.com/opensourceways/server-common-lib v0.0.0-20231016134644-4bd7efe825a9
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/IBM/sarama v1.43.2 // indirect
+	github.com/antihax/optional v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dnwe/otelsarama v0.0.0-20240308230250-9388d9d40bc0 // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/oauth2 v0.4.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.26.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
---
+++ robot.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	kfklib "github.com/opensourceways/kafka-lib/agent"
+
+	"github.com/opensourceways/community-robot-lib/config"
+	framework "github.com/opensourceways/community-robot-lib/robot-gitee-framework"
+	sdk "github.com/opensourceways/go-gitee/gitee"
+	"github.com/sirupsen/logrus"
+)
+
+const botName = "message-collect-githook"
+
+func newRobot() *robot {
+	return &robot{}
+}
+
+type robot struct{}
+
+func (bot *robot) NewConfig() config.Config {
+	return &configuration{}
+}
+
+func (bot *robot) getConfig(cfg config.Config, org, repo string) (*botConfig, error) {
+	c, ok := cfg.(*configuration)
+	if !ok {
+		return nil, fmt.Errorf("can't convert to configuration")
+	}
+	if bc := c.configFor(org, repo); bc != nil {
+		return bc, nil
+	}
+
+	return nil, fmt.Errorf("no config for this repo:%s/%s", org, repo)
+}
+
+func (bot *robot) RegisterEventHandler(p framework.HandlerRegitster) {
+	p.RegisterIssueHandler(bot.handleIssueEvent)
+	p.RegisterPullRequestHandler(bot.handlePREvent)
+	p.RegisterNoteEventHandler(bot.handleNoteEvent)
+	p.RegisterPushEventHandler(bot.handlePushEvent)
+}
+func (bot *robot) handlePREvent(e *sdk.PullRequestEvent, c config.Config, log *logrus.Entry) error {
+	body, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+	if *e.Action == "update" || *e.Action == "set_draft" || *e.Action == "cancel_draft" {
+		log.Info("Ignore update events and set to draft.")
+		return nil
+	}
+	log.Info("handle pr event,send kafka gitee_pr_raw")
+	return kfklib.Publish("gitee_pr_raw", nil, body)
+}
+func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, c config.Config, log *logrus.Entry) error {
+	body, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+	log.Info("handle note event,send kafka gitee_note_raw")
+	if e.Issue != nil && e.Issue.TypeName == "CVE和安全问题" {
+		return kfklib.Publish("cve_note_raw", nil, body)
+	} else {
+		return kfklib.Publish("gitee_note_raw", nil, body)
+	}
+}
+
+func (bot *robot) handlePushEvent(e *sdk.PushEvent, c config.Config, log *logrus.Entry) error {
+	body, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+	log.Info("handle push event,send kafka gitee_push_raw")
+	return kfklib.Publish("gitee_push_raw", nil, body)
+}
+
+func (bot *robot) handleIssueEvent(e *sdk.IssueEvent, c config.Config, log *logrus.Entry) error {
+	body, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+	log.Info("handle issue event,send kafka gitee_issue_raw")
+	if e.Issue != nil && e.Issue.TypeName == "CVE和安全问题" {
+		return kfklib.Publish("cve_issue_raw", nil, body)
+	} else {
+		return kfklib.Publish("gitee_issue_raw", nil, body)
+	}
+}
